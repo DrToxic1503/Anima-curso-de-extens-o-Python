452bdec951f26a5880ed2930e5d6c06ae7705476
João Pedro Reis Neves <82532653+DrToxic1503@users.noreply.github.com>
2022-10-20 01:19:43 +0000

    Três formas de exibir variáveis e duas variáveis em uma mesma ordem de print.

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 # meu primeiro projeto python 
 #
-print ("Alo mundo deuses do python me ajudem")
+# print ("") = comando de saida
 
+print ("Alo mundo e que os deuses do python me ajudem")
+
+# guardar uma string (frase)
+# acentuação apenas entre ""
+
+nome = "João Neves"
+
+# guardar um numero inteiro
+
+idade = 19
+
+# exibir variável nome
+
+print ("Meu nome é: " + nome)
+
+# print "minha idade é:"
+# adicionar " + e str" (por ser um número inteiro)
+# ou adicionar {} dentro das aspas em seguida .format() fora das aspas
+
+print ("Minha idade é: " + str (idade) + " anos")
+print ("Minha idade é: {} anos" .format(idade))
+print (f"Minha idade é: {idade} anos")
+
+# exibir nome + idade
+
+print("Meu nome é: {} e tenho: {} anos".format (nome, idade))
