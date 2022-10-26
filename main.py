1025975ca8b846210e8c0a038fd246c5308a2dd6
João Pedro Reis Neves <82532653+DrToxic1503@users.noreply.github.com>
2022-10-26 23:13:11 +0000

    Novo comando inpu (in e out) e print o dobro da idade informada

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# AULA 1    19/10/2022
+
 # meu primeiro projeto python 
 #
 # print ("") = comando de saida
@@ -28,3 +30,19 @@ print (f"Minha idade é: {idade} anos")
 # exibir nome + idade
 
 print("Meu nome é: {} e tenho: {} anos".format (nome, idade))
+
+# AULA 2   26/10/2022
+
+# comando input(): quero permitir que o usuário digite algo "in"
+
+nome = input ("Digite o seu nome: ")
+idade = int (input ("Digite a sua idade: "))
+
+# comando input(): out
+print (f"Bem vindo, {nome}")
+print (f"Você tem: {idade} anos de idade")
+
+# mostrar o dobro da idade informada
+
+dobro = idade * 2
+print (f"O dobro da sua idade é: {dobro}")
