7f329fdd2995492d3e072c55d75f4f2ba21dfe07
João Pedro Reis Neves <82532653+DrToxic1503@users.noreply.github.com>
2022-10-27 01:14:36 +0000

    Resto da aula 2 if, else, elif

--- main.py
+++ main.py
@@ -46,3 +46,34 @@ print (f"Você tem: {idade} anos de idade")
 
 dobro = idade * 2
 print (f"O dobro da sua idade é: {dobro}")
+
+# estrutura condicional if (se) 
+# "if" a pessoa for de maior "else" você é maior de idade, ja pode dirigir
+
+if idade >= 18:
+  print ("Você é maior de idade, ja pode dirigir")
+else:
+  print ("Você é jovem ainda")
+
+# questionar o genero (M = masculino e F = feminino)
+# if (vc precisa ou precisou prestar o serviço militar obrigatório)
+
+genero = input ("Informe o seu gênero M - Masculino  ou F - Feminino: ")
+if idade >= 18 and genero == "M":
+  print("Você precisa ou precisou prestar o serviço militar obrigatório")
+
+# exercício de fixação
+
+nome = input("Qual o seu nome? ")
+nota = int(input("Qual a sua nota? "))
+
+if (nota == 10):
+  print (f"{nome}, Você é o bixão mesmo!")
+elif (nota >= 6 and nota < 10):
+  print (f"{nome}, bom trabalho")
+else:
+  print ("Burro, não tirou nota dez")
+
+print ("executada de qlqr jeito")
+
+
